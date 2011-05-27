7a3d93ea69c458f11fae8d60d3a814a3f61a7efc
FM <matthew.finkel@gmail.com>
2011-05-27 15:08:22 -0400

    Initial commit

---
+++ src/androidapp/GuardtheBridge/GuardtheBridge.java
@@ -0,0 +1,17 @@
+package androidapp.GuardtheBridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GuardtheBridge extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Please Follow the Directions Below.");
+        setContentView(tv);
+    }
+}
