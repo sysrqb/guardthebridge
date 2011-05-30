1267f6338fa446c5c3101593c807ea8e9bcf1451
FM <matthew.finkel@gmail.com>
2011-05-30 02:21:19 -0400

    Login layout in main.xml

--- gen/androidapp/GuardtheBridge/R.java
+++ gen/androidapp/GuardtheBridge/R.java
@@ -13,11 +13,35 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int authkey=0x7f050006;
+        public static final int authtext=0x7f050005;
+        public static final int carnum=0x7f050007;
+        public static final int directions=0x7f050002;
+        public static final int dispatch=0x7f050000;
+        public static final int emergency=0x7f050001;
+        public static final int login=0x7f050008;
+        public static final int netid=0x7f050004;
+        public static final int netidtext=0x7f050003;
+        public static final int text1=0x7f050009;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activelist=0x7f030000;
+        public static final int carnums=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int rides=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040007;
+        public static final int authtext=0x7f040004;
+        public static final int carnum=0x7f040005;
+        public static final int directions=0x7f040002;
+        public static final int dispatch=0x7f040009;
+        public static final int emergency=0x7f04000a;
+        public static final int login=0x7f040006;
+        public static final int minvers=0x7f040000;
+        public static final int netidtext=0x7f040003;
+        public static final int no_rides=0x7f040008;
+        public static final int targetvers=0x7f040001;
     }
 }
--- src/androidapp/GuardtheBridge/GuardtheBridge.java
+++ src/androidapp/GuardtheBridge/GuardtheBridge.java
@@ -2,16 +2,24 @@ package androidapp.GuardtheBridge;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GuardtheBridge extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        TextView tv = new TextView(this);
-        tv.setText("Please Follow the Directions Below.");
-        setContentView(tv);
+        setContentView(R.layout.activelist);
+        
+        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        		this, R.array.carnums, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
+    
+    public boolean onOptionItemSelected(MenuItem menu){
+		return super.onOptionsItemSelected(menu);
     }
 }
