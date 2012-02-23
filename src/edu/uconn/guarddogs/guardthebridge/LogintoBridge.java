745dd0718cf72cb551ab73157fecad2ac3d3900a
FM <matthew.finkel@gmail.com>
2012-02-23 12:25:20 -0500

    Authentication is not handled in a background thread

--- src/edu/uconn/guarddogs/guardthebridge/LogintoBridge.java
+++ src/edu/uconn/guarddogs/guardthebridge/LogintoBridge.java
@@ -30,8 +30,10 @@ import com.google.protobuf.TextFormat;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -49,6 +51,7 @@ public class LogintoBridge extends ListActivity {
     private CarsGtBDbAdapter mDbHelper;
     private static final int CarNum_SELECT=0;
     private LogintoBridge self;
+    private ProgressDialog mProgBar = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -95,14 +98,7 @@ public class LogintoBridge extends ListActivity {
 	        loginButton.setOnClickListener(new View.OnClickListener() {
 
 	            public void onClick(View view) {
-	            	int retval;
-	            	if((retval = sendAuthCheck(mNetIdText, mAuthText, mCarNum)) != 0){
-	            		dealwitherrors(retval);
-	            	}
-	            	else{
-		                Intent i = new Intent (self, GuardtheBridge.class);
-		                startActivity(i);
-	            	}
+	            	new AuthTask().execute();
 	            }
 
 	        });
@@ -120,108 +116,172 @@ public class LogintoBridge extends ListActivity {
 
 	        });
 	    }
+	 
+	 private class AuthTask extends AsyncTask<Void, Integer, Integer>
+	 {
+		 static final int INCREMENT_PROGRESS = 10;
+		 protected void onPreExecute()
+		 {
+			 mProgBar = new ProgressDialog(self);
+			 mProgBar.setCancelable(true);
+			 mProgBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			 mProgBar.setMessage("Establishing Connection with server...");
+			 mProgBar.show();
+		 }
+		 
+		 @Override
+		 protected Integer doInBackground(Void... params)
+		 {
+			 int nRetVal = sendAuthCheck(mNetIdText, mAuthText, mCarNum);
+			 return nRetVal;
+		 }
+ 
+		 public int sendAuthCheck(EditText netid, EditText authcode, String carnum)
+		 {
+			 Request aPBReq;
+			 Response aPBRes;
+			 GtBSSLSocketFactoryWrapper aSSLSF = new GtBSSLSocketFactoryWrapper();
+			 
+			 publishProgress(INCREMENT_PROGRESS);
+			 SSLSocket aSock = aSSLSF.getSSLSocket();
+			 if(aSock.isClosed())
+			 {
+				 aSock = aSSLSF.createSSLSocket(self);
+			 }
+			 if (aSock.getSession() == null)
+			 {
+				 aSSLSF = aSSLSF.getNewSSLSFW(self);
+				 aSock = aSSLSF.getSSLSocket();
+			 }
+			 
+			 publishProgress(INCREMENT_PROGRESS);
+			 try {
+				 OutputStream aOS = null;
+				 aOS = aSock.getOutputStream();
+				 aPBReq = Request.newBuilder().
+						 setNReqId(2).
+						 setSReqType("AUTH").
+						 addSParams(netid.getText().toString()).
+						 addSParams(authcode.getText().toString()).
+						 addSParams(carnum).
+						 build();
+				 Log.v(TAG, "Number of Params: " + aPBReq.getSParamsCount());
+				 Log.v(TAG, "Nightly Key: " + authcode.getText().toString());
+				 Log.v(TAG, "NetID: " + netid.getText().toString());
+				 Log.v(TAG, "Car Number: " + carnum);
+				 Log.v(TAG, "Serialized Size: " + aPBReq.getSerializedSize());
+				 Log.v(TAG, "Sending Buf:");
+				 Log.v(TAG, TextFormat.shortDebugString(aPBReq));
+				 try
+				 {
+					 aOS.write(aPBReq.getSerializedSize());
+				 } catch (SSLProtocolException e)
+				 {
+					 Log.e(TAG, "SSLProtoclException Caught. On-write to Output Stream");
+					 aSSLSF.forceReHandshake(self);
+					 aSock = aSSLSF.getSSLSocket();
+					 aOS = aSock.getOutputStream();
+					 try
+					 {
+						 aOS.write(aPBReq.getSerializedSize());
+					 } catch (SSLProtocolException ex)
+					 {
+						 aSSLSF = aSSLSF.getNewSSLSFW(self);
+						 aSock = aSSLSF.getSSLSocket();
+						 aOS = aSock.getOutputStream();
+						 aOS.write(aPBReq.getSerializedSize());
+					 }
+				 }
+				 publishProgress(INCREMENT_PROGRESS);
+				 aPBReq.writeTo(aOS);
+				 aOS.close();
+				 InputStream aIS = aSock.getInputStream();
+				 byte[] vbuf = new byte[14];
+				 aIS.read(vbuf);
 	
-	public int sendAuthCheck(EditText netid, EditText authcode, String carnum){
-		Request aPBReq;
-		Response aPBRes;
-		GtBSSLSocketFactoryWrapper aSSLSF = new GtBSSLSocketFactoryWrapper();
-		
-		SSLSocket aSock = aSSLSF.getSSLSocket();
-		if(aSock.isClosed())
-		{
-			aSock = aSSLSF.createSSLSocket(this);
-		}
-		if (aSock.getSession() == null)
-		{
-			aSSLSF = aSSLSF.getNewSSLSFW(this);
-			aSock = aSSLSF.getSSLSocket();
-		}
-			
-		try {
-			OutputStream aOS = null;
-				aOS = aSock.getOutputStream();
-			aPBReq = Request.newBuilder().
-				setNReqId(2).
-				setSReqType("AUTH").
-				addSParams(netid.getText().toString()).
-				addSParams(authcode.getText().toString()).
-				addSParams(carnum).
-				build();
-			Log.v(TAG, "Number of Params: " + aPBReq.getSParamsCount());
-			Log.v(TAG, "Nightly Key: " + authcode.getText().toString());
-			Log.v(TAG, "NetID: " + netid.getText().toString());
-			Log.v(TAG, "Car Number: " + carnum);
-			Log.v(TAG, "Serialized Size: " + aPBReq.getSerializedSize());
-			Log.v(TAG, "Sending Buf:");
-			Log.v(TAG, TextFormat.shortDebugString(aPBReq));
-			try 
-			{
-				aOS.write(aPBReq.getSerializedSize());
-			}catch (SSLProtocolException e)
-			{
-				Log.e(TAG, "SSLProtoclException Caught. On-write to Output Stream");
-				aSSLSF.forceReHandshake(this);
-				aSock = aSSLSF.getSSLSocket();
-				aOS = aSock.getOutputStream();
-				try
-				{
-					aOS.write(aPBReq.getSerializedSize());
-				} catch (SSLProtocolException ex)
-				{
-					aSSLSF = aSSLSF.getNewSSLSFW(this);
-					aSock = aSSLSF.getSSLSocket();
-					aOS = aSock.getOutputStream();
-					aOS.write(aPBReq.getSerializedSize());
-				}
-			}
-			aPBReq.writeTo(aOS);
-			aOS.close();
-			InputStream aIS = aSock.getInputStream();
-			byte[] vbuf = new byte[14];
-			aIS.read(vbuf);
-			
-			aSock.close();
-			aPBRes = Response.parseFrom(vbuf);
-			aIS.close();
-			return aPBRes.getNRespId();
-		} catch (IOException e){
-			e.printStackTrace();
-			getConnFailedDialog("Connection to server could not be established. " + 
-					"Please try again in a minute or call Dispatch.");
-			return -2;
-		}
-	}
-	
-	public void dealwitherrors(int retval){
-		switch (retval){
-		case -1:
-			getConnFailedDialog("I'm sorry, but please retype the authentication code." 
-					+ "The one you entered could not be verified");
-			break;
-		case -2:
-			getConnFailedDialog("I'm sorry, but please retype your NetID." 
-					+ "The one you entered could not be verified");
-			break;
-		case -3:
-			
-		case -4:
-			
-		default:
-			getConnFailedDialog("I'm sorry, but an unknown error occurred. " +
-					"Please call dispatch/the supervisor if this persists.");
-			break;
-		}
-	}
-	
-	private void getConnFailedDialog(String msg){
-		Log.w(TAG, "Failed to Connect to server");
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(msg);
-		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-           public void onClick(DialogInterface dialog, int id) {
-           	    return;
-           }
-        });
-		builder.show();
-	}
+				 aSock.close();
+			 	aPBRes = Response.parseFrom(vbuf);
+			 	publishProgress(INCREMENT_PROGRESS);
+			 	aIS.close();
+			 	return aPBRes.getNRespId();
+			 } catch (IOException e){
+				 Log.e(TAG, "Connection to server could not be established.");
+				 return -2;
+			 }
+		 }
+	 
+		 protected void onPostExecute(Integer res)
+		 {
+			 publishProgress(INCREMENT_PROGRESS);
+			 if(res != 0)
+			 {
+				 dealwitherrors(res);
+			 }
+			 else{
+				 mProgBar.dismiss();
+				 Intent i = new Intent (self, GuardtheBridge.class);
+				 startActivity(i);
+			 }
+		 }
+	 	
+		 protected void onProgressUpdate(Integer... progress)
+		 {
+			 int nTotalProgress = mProgBar.getProgress() + progress[0];
+			 switch (nTotalProgress)
+			 {
+			 case 0:
+			 case 20:
+				 mProgBar.setMessage("Establishing Connection with server...");
+				 break;
+			 case 40:
+				 mProgBar.setMessage("Connection Established, Sending credentials...");
+				 break;
+			 case 60:
+				 mProgBar.setMessage("Receiving response...");
+				 break;
+			 case 80:
+				 mProgBar.setMessage("Reading response...");
+				 break;
+			 case 100:
+				 mProgBar.setMessage("Done!");
+				 break;
+			 }		
+			 mProgBar.setProgress(nTotalProgress);
+		 }
+		 
+		 public void dealwitherrors(int retval)
+		 {
+			 mProgBar.dismiss();
+			 switch (retval)
+			 {
+			 case -1:
+				 getDialog("I'm sorry, but please retype the authentication code." 
+						 + "The one you entered could not be verified");
+				 break;
+			 case -2:
+				 getDialog("I'm sorry, but please retype your NetID." 
+						 + "The one you entered could not be verified");
+				 break;
+			 case -3:
+				 
+			 case -4:
+				 
+			 default:
+				 getDialog("I'm sorry, but an unknown error occurred. " +
+						 "Please call dispatch/the supervisor if this persists.");
+				 break;
+			 }
+		 }
+
+		 private void getDialog(String msg){
+			 AlertDialog.Builder builder = new AlertDialog.Builder(self);
+			 builder.setMessage(msg);
+			 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				 public void onClick(DialogInterface dialog, int id) {
+					 return;
+				 }
+			 });
+			 builder.show();
+		 }
+	 }
 }
